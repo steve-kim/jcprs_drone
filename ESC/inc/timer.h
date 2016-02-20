dc6f387553fdd46beb1a507b251b78f3b289a12f
Steve Kim <ElSteve05@gmail.com>
2016-02-19 21:32:58 -0800

    Initial commit for timer module
    I will be using timer interrupts to implement the first pass of motor
    control. A timer will fire periodically, and trigger a change in state.

---
+++ ESC/inc/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: steve-kim
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+void initializeTimer();
+
+#endif /* INC_TIMER_H_ */
---
+++ ESC/src/timer.c
@@ -0,0 +1,21 @@
+/*
+ * timer.c
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: steve-kim
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+
+void initializeTimer() {
+
+}
+
