da541526af56b0d04ccaa1d0a55a18d37ef97e31
Steve Kim <ElSteve05@gmail.com>
2016-02-19 19:20:41 -0800

    Minimal viable effort to verify PWM outputs
    Created a very simple program that simply exercises 2 PWM outputs. Also
    made some modifications to the Eclipse project to resolve compilation
    issues.

---
+++ ESC/src/main.c
@@ -0,0 +1,100 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: steve-kim
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/sysctl.h"
+
+int main(void) {
+	// Set up system clock
+	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+            SYSCTL_XTAL_16MHZ);
+
+    //
+    // Set the PWM clock to the system clock.
+    //
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+
+    //
+    // The PWM peripheral must be enabled for use.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+
+    //
+	// For this example PWM0 is used with PortB Pins 6 and 7.  The actual port
+	// and pins used may be different on your part, consult the data sheet for
+	// more information.  GPIO port B needs to be enabled so these pins can be
+	// used.
+	// TODO: change this to whichever GPIO port you are using.
+	//
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+    //
+    // Configure the GPIO pin muxing to select PWM functions for these pins.
+    // This step selects which alternate function is available for these pins.
+    // This is necessary if your part supports GPIO pin function muxing.
+    // Consult the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinConfigure(GPIO_PB6_M0PWM0);
+    GPIOPinConfigure(GPIO_PB7_M0PWM1);
+
+    //
+    // Configure the GPIO pad for PWM function on pins PB6 and PB7.  Consult
+    // the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6);
+    GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_7);
+
+    //
+    // Configure the PWM0 to count up/down without synchronization.
+    // Note: Enabling the dead-band generator automatically couples the 2
+    // outputs from the PWM block so we don't use the PWM synchronization.
+    //
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_UP_DOWN |
+                    PWM_GEN_MODE_NO_SYNC);
+
+    //
+    // Set the PWM period to 250Hz.  To calculate the appropriate parameter
+    // use the following equation: N = (1 / f) * SysClk.  Where N is the
+    // function parameter, f is the desired frequency, and SysClk is the
+    // system clock frequency.
+    // In this case you get: (1 / 250Hz) * 16MHz = 64000 cycles.  Note that
+    // the maximum period you can set is 2^16 - 1.
+    // TODO: modify this calculation to use the clock frequency that you are
+    // using.
+    //
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, 64000);
+
+    //
+    // Set PWM0 PD0 to a duty cycle of 25%.  You set the duty cycle as a
+    // function of the period.  Since the period was set above, you can use the
+    // PWMGenPeriodGet() function.  For this example the PWM will be high for
+    // 25% of the time or 16000 clock cycles (64000 / 4).
+    //
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0,
+                     PWMGenPeriodGet(PWM0_BASE, PWM_GEN_0) / 4);
+
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1,
+                         PWMGenPeriodGet(PWM0_BASE, PWM_GEN_0) / 2);
+    //
+    // Enable the PWM0 Bit 0 (PD0) and Bit 1 (PD1) output signals.
+    //
+    PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT | PWM_OUT_0_BIT, true);
+
+    //
+    // Enables the counter for a PWM generator block.
+    //
+    PWMGenEnable(PWM0_BASE, PWM_GEN_0);
+
+    while(true) {}
+
+}
